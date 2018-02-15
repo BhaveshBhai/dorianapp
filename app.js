fd2037fa78309d384949911609f3166199293c2c
Bhavesh <osi.bhavesh@gmail.com>
2018-02-15 16:52:50 +0530

    fde
    df

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js")(app);
 
 app.use('/', express.static(__dirname + '/public/'));
-app.listen(process.env.PORT || 5000);
+//app.listen(process.env.PORT || 5000);
 
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
